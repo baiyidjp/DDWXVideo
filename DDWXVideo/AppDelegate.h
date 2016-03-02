d7ba9c917ec87263004e90809b78e92303be21a6
baiyidjp <tztddong@dongjiangpengdemac-mini.local>
2016-03-02 15:04:35 +0800

    Initial Commit

---
+++ DDWXVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDWXVideo
+//
+//  Created by tztddong on 16/3/2.
+//  Copyright © 2016年 dongjiangpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDWXVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DDWXVideo
+//
+//  Created by tztddong on 16/3/2.
+//  Copyright © 2016年 dongjiangpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
