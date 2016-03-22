8b59bc9da04f2e21e85f8bff381ba86494c00945
baiyidjp <tztddong@dongjiangpengdeMac-mini.local>
2016-03-22 11:49:42 +0800

    1.0
    仿微信录制小视频
    可定制

---
+++ DDWXVideo/Public.h
@@ -0,0 +1,23 @@
+//
+//  Public.h
+//  ihavecar
+//
+//  Created by tztddong on 16/2/23.
+//  Copyright © 2016年 dongjiangpeng. All rights reserved.
+//
+
+#ifndef Public_h
+#define Public_h
+
+#define RGB(r,g,b) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1.0]
+#define KWIDTH  [UIScreen mainScreen].bounds.size.width
+#define KHEIGHT [UIScreen mainScreen].bounds.size.height
+#define TOPNAVH 64
+#define CELLH 44
+#define KMARGIN 8
+#define BUTTONCOUNT 10
+#define FONT(x)  [UIFont systemFontOfSize:x]
+
+#import "UIView+YYAdd.h"
+
+#endif /* Public_h */
--- DDWXVideo/ViewController.m
+++ DDWXVideo/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "DDWXVideoController.h"
+#import<MobileCoreServices/MobileCoreServices.h>
 
-@interface ViewController ()
+@interface ViewController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 
 @end
 
@@ -17,8 +19,56 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(50, 200, 100, 30)];
+    [btn setTitle:@"点我" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
 }
 
+- (void)click{
+    
+    UIAlertController *sheetCtrl = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    [sheetCtrl addAction:[UIAlertAction actionWithTitle:@"本地视频" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+        [self locaClick];
+    }]];
+    [sheetCtrl addAction:[UIAlertAction actionWithTitle:@"录制视频" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+        [self takeClick];
+    }]];
+    [sheetCtrl addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil]];
+    
+    [self presentViewController:sheetCtrl animated:YES completion:nil];
+}
+
+- (void)takeClick{
+    
+    DDWXVideoController *ctrl = [[DDWXVideoController alloc]init];
+    ctrl.allTime = 10;
+    ctrl.minTime = 1.0;
+    [self presentViewController:ctrl animated:YES completion:nil];
+    
+}
+
+- (void)locaClick{
+    
+    UIImagePickerController *Videopicker = [[UIImagePickerController alloc]init];
+    Videopicker.delegate = self;
+    [Videopicker setEditing:NO];
+    Videopicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    Videopicker.mediaTypes = [NSArray arrayWithObject:(NSString *)kUTTypeMovie];
+    [self presentViewController:Videopicker animated:YES completion:nil];
+    
+}
+
+#pragma mark  imagePickerController
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    NSLog(@"%@",[info valueForKey:UIImagePickerControllerMediaURL]);
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
